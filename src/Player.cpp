ba50895c27b69619229e78644566ab809cdca73b
stephschuster <stephs@gefen.online>
2016-11-23 18:20:14 +0200

    add player

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,7 +2,12 @@
 #include<Player.h>
 #include <map>
 #include "../include/Player.h"
+#include "../include/Hand.h"
 
 string Player::getName(){
     return name;
+}
+
+string Player::toString() {
+    return Player::getName()+ ": " + Hand::toString();
 }
