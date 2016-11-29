b37ed70013b889f5b45e525aed7d76b720d92bae
stephschuster <stephs@gefen.online>
2016-11-29 22:30:58 +0200

    remove unused functions

--- include/Hand.h
+++ include/Hand.h
@@ -25,8 +25,6 @@ public:
 	Hand(const Hand& other);
     bool addCard(Card &card);
     bool removeCard(Card &card);
-	pair<bool,int> checkForNumericSerias();//returns true is there is serias & the repeated card , false otherwise
-	pair<bool,Figure> checkForFigureSerias();
     int getNumberOfCards(); // Get the number of cards in hand
     string toString(); // Return a list of the cards, separated by space, in one line, in a sorted order, ex: "2S 5D 10H"
 	virtual ~Hand()=0;
