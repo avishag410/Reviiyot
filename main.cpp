e172e3c4b764535e47f21008522d764e6a1cd7fe
shooga <avishag212@gmail.com>
2016-12-01 03:47:20 +0200

    fixed some mem alloc probs

--- main.cpp
+++ main.cpp
@@ -10,19 +10,20 @@ int main(int argc,char **argv)
 {
     char* configurationFile = argv[1];
 
-    Game game = Game(configurationFile);
-    game.init();
-    Game initializedGame = game;
-    game.play();
+    Game * game = new Game(configurationFile);
+    game->init();
+    Game * initializedGame = game; // not calling copy ctr
+    game->play();
 
     cout << std::endl;
-    game.printWinner();
-    game.printNumberOfTurns();
+    game->printWinner();
+    game->printNumberOfTurns();
     cout << "----------" << endl;
     cout<<"Initial State:"<<endl;
-    initializedGame.printState();
+    initializedGame->printState();
     cout<<"----------"<<endl;
     cout<<"Final State:"<<endl;
-    game.printState();
+    game->printState();
+	delete(game);
     return 0;
 }
